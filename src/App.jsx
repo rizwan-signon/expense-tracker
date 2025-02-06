8b7590123c7f2d2aa00aaba31a9d46b5da7d6ff3
rizwan.ullah <rizwan.ullah@plcgroup.com>
2025-02-06 08:41:21 -0500

    manage state in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
 
 const App = () => {
+  const [expenses, setExpenses] = useState([]);
+
+  const addExpense = (expense) => {
+    setExpenses([...expenses, expense]);
+  };
+
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar />
+      <div className="max-w-xl mx-auto p-4">
+        <ExpenseForm onAddExpense={addExpense} />
+        <ExpenseList expenses={expenses} />
+      </div>
     </div>
   );
 };
